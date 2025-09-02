943618c721f6dfc64fbade0766e4acca8fb253e2
hoanghaix86 <hai.hoang.2762@gmail.com>
2025-09-02 17:47:50 +0700

    feat: create and delete qemu simple

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/hoanghaix86/proxmox-api-go/proxmox/client"
+	"github.com/hoanghaix86/proxmox-api-go/proxmox/core"
+)
 
 func main() {
 	fmt.Println("PROXMOX API GO BETA")
+
+	ctx := context.Background()
+
+	client := client.NewClient(nil, nil)
+	client.AuthWithToken(nil, nil)
+
+	vm := core.QEMU{
+		Id:   300,
+		Node: "proxmox",
+	}
+
+	// upid := vm.Create(ctx, client)
+	// fmt.Println(*upid)
+	upid := vm.Delete(ctx, client, nil)
+	fmt.Println(*upid)
 }
--- proxmox/core/qemu.go
+++ proxmox/core/qemu.go
@@ -1,24 +1,69 @@
 package core
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/hoanghaix86/proxmox-api-go/proxmox/client"
+)
+
 type UPID string
 
 type QEMU struct {
 	Id   uint64 `json:"id"`
-	Node uint64 `json:"node"`
+	Node string `json:"node"`
+
+	Boot    string `json:"boot,omitempty"`    // computed
+	VmGenId string `json:"vmgenid,omitempty"` // computed
+	Meta    string `json:"meta,omitempty"`    // computed
+	SmBios1 string `json:"smbios1,omitempty"` // computed
 }
 
-func (q *QEMU) Create() (*UPID, error) {
+type CreateQEMURequest struct {
+	Id   uint64 `json:"vmid"`
+	Node string `json:"node"`
+}
+
+func (q *QEMU) ToCreateQEMURequest() *CreateQEMURequest {
+	return &CreateQEMURequest{
+		Id:   q.Id,
+		Node: q.Node,
+	}
+}
+
+func (q *QEMU) Create(ctx context.Context, c *client.Client) *UPID {
+	path := fmt.Sprintf("/nodes/%s/qemu", q.Node)
+	upid := client.Post[UPID](ctx, c, path, nil, q.ToCreateQEMURequest())
+	return upid
+}
+
+type DeleteQEMUArgs struct {
+	DestroyUnreferencedDisks bool `url:"destroy-unreferenced-disks,omitempty"`
+	Purge                    bool `url:"purge,omitempty"`
+	SkipLock                 bool `url:"skiplock,omitempty"`
+}
+
+func NewDeleteQEMUArgs() *DeleteQEMUArgs {
+	return &DeleteQEMUArgs{
+		DestroyUnreferencedDisks: true,
+		Purge:                    true,
+		SkipLock:                 false,
+	}
+}
+
+func (q *QEMU) Delete(ctx context.Context, c *client.Client, args *DeleteQEMUArgs) *UPID {
+	path := fmt.Sprintf("/nodes/%s/qemu/%d", q.Node, q.Id)
+	if args == nil {
+		args = NewDeleteQEMUArgs()
+	}
+	upid := client.Delete[UPID](ctx, c, path, args, nil)
+	return upid
+}
+
+func (q *QEMU) GetConfig(ctx context.Context, c *client.Client) (*UPID, error) {
 	return nil, nil
 }
 
-func (q *QEMU) Delete() (*UPID, error) {
-	return nil, nil
-}
-
-func (q *QEMU) GetConfig() (*UPID, error) {
-	return nil, nil
-}
-
-func (q *QEMU) UpdateConfig() (*UPID, error) {
+func (q *QEMU) UpdateConfig(ctx context.Context, c *client.Client) (*UPID, error) {
 	return nil, nil
 }
